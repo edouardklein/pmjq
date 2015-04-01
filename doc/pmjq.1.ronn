bada29f426497a247ea6f03d93c8acd31c3e75bc
Edouard Klein <edouardklein@gmail.com>
2015-04-01 14:27:29 +0200

    Now actually archives executed files

--- doc/pmjq.1.ronn
+++ doc/pmjq.1.ronn
@@ -9,11 +9,30 @@ pmjq(1) -- watch and process a job queue
 
 The `pmjq` utility watches the directory <job-spool> and executes the scripts that exist in it. Once executed, the script is copied in the directory <archive-dir> if specified.
 
+Users submit jobs by writing an executable file in the <job-spool> directory. Therefore, a minimal pmjq server is just a file server with write-access enabled. Users are responsible for choosing a naming scheme for their jobs that will avoid conflicts and collisions.
+
+Running more than one instance of pmjq by server only makes sense if they are watching different pools. Multiple machines can mount the <job-spool> directory into their namespace and run `pmjq`, thus distributing the jobs among nodes of a cluster.
+
+`pmjq` can not watch more than one pool at once. Funny business should be handled at the filesystem level, with e.g. something similar to Plan 9 bind(2).
+
 The arguments are as follows:
 
 * `-o`
-Specify the archive directory, in which executed scripts are copied.
+Specify the archive directory, in which executed scripts are copied. A timestamp prefix is prepended to the name of the file. Jobs with the same name that finish executing at the same second will lead to data loss : one will overwrite the other. Users are responsible for choosing appropriate names.
 
 * `-C`
 Specify (as a percentage) a CPU limit. No new job is launched if the current CPU usage is above the limit.
 
+## TODO
+
+* Security considerations : allow jobs to be ran with sudo -u file-owner
+
+* Allow a command to be run (e.g. shutting down the machine) if no jobs are available for a certain amount of time
+
+* Allow a command to be run (e.g. booting new nodes) if one of the limit is reached for a certain amount of time
+
+## BUGS
+
+* `pmjq` has no mechanism to protect against name collisions in the <job-spool> directory, and only minimal (a timestamp) mechanism in the <archive-dir> directory
+
+* The security is very very bad, write access to the file server should only be granted to trusted users.
--- pmjq.go
+++ pmjq.go
@@ -1,54 +1,63 @@
 package main
 
 // #include <sys/file.h>
-import "C" 
+import "C"
 
 import (
-	"log"
-	"github.com/looplab/fsm"
 	"github.com/docopt/docopt-go"
-	"time"
+	"github.com/looplab/fsm"
+	"io/ioutil"
+	"log"
 	"os"
 	"os/exec"
-	"io/ioutil"
+	"path"
 	"syscall"
+	"time"
 )
 
 type State struct {
 	main_loop *fsm.FSM
 }
 
-func execution(e *fsm.Event, events chan<-string, jobs <-chan*os.File) {
+func execution(e *fsm.Event, archive_dir string, events chan<- string, jobs <-chan *os.File) {
 	log.Println("Beginning exec")
-	file := <- jobs
-	log.Printf("File to exec : %s\n",file.Name())
-	cmd := exec.Command("./"+file.Name())
+	file := <-jobs
+	log.Printf("File to exec : %s\n", file.Name())
+	cmd := exec.Command("./" + file.Name())
 	err := cmd.Start()
 	if err != nil {
 		log.Fatal(err)
 	}
-	go func(cmd *exec.Cmd,f *os.File){
+	go func(cmd *exec.Cmd, f *os.File) {
 		log.Printf("GOROUTINE THAT WAITS FOR THE JOB %s TO FINISH\n", f.Name())
 		err = cmd.Wait()
 		log.Printf("Command finished with error: %v... ", err)
-		log.Printf("DONE, UNLINKING AND CLOSING FILE %",f.Name())
+		if archive_dir != "" {
+			log.Println("Copying file to archive dir")
+			err := exec.Command("cp", f.Name(),
+				archive_dir+"/"+time.Now().Local().Format("20060102-15:04:05-")+path.Base(f.Name())).Run()
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+		log.Printf("DONE, UNLINKING AND CLOSING FILE %", f.Name())
 		syscall.Unlink(f.Name())
 		f.Close()
-	}(cmd, file)	
+	}(cmd, file)
 	log.Println("Finished launching")
-	go func(){events<-"launched"}()
+	go func() { events <- "launched" }()
 }
 
 func book_keeping(e *fsm.Event, c chan<- string) {
 	//log.Println("Beginning book keeping")
 	//log.Println("Finished book keeping")
-	go func(){c<-"books kept"}()
+	go func() { c <- "books kept" }()
 }
 
-func get_lock(filename string) (*os.File){
+func get_lock(filename string) *os.File {
 	log.Println("\tTrying to get a lock...")
-	file,_ := os.Open(filename) //FIXME:Check err
-	err := syscall.Flock(int(file.Fd()), C.LOCK_EX + C.LOCK_NB)
+	file, _ := os.Open(filename) //FIXME:Check err
+	err := syscall.Flock(int(file.Fd()), C.LOCK_EX+C.LOCK_NB)
 	if err != nil { //Unable to obtain lock
 		log.Println("\tFile already locked")
 		file.Close()
@@ -60,33 +69,34 @@ func get_lock(filename string) (*os.File){
 func polling(e *fsm.Event, spool_dir string, events chan<- string, jobs chan<- *os.File) {
 	log.Println("Beginning polling")
 	entries, _ := ioutil.ReadDir(spool_dir) //FIXME:Check err
-	for _,file_info := range entries{
+	for _, file_info := range entries {
 		log.Printf("Analyzing file: \t %s\n", file_info.Name())
 		//FIXME: check x permission
-		file := get_lock(spool_dir+"/"+file_info.Name())
-		if file != nil{
+		file := get_lock(spool_dir + "/" + file_info.Name())
+		if file != nil {
 			log.Printf("\tJob found: %s\n", file_info.Name())
-			go func(){jobs <- file}()
-			go func(){events <- "job found"}()
+			go func() { jobs <- file }()
+			go func() { events <- "job found" }()
 			return
 		}
 	}
 	log.Println("Done polling")
-	go func(){events<-"no jobs found"}()
-	
+	go func() { events <- "no jobs found" }()
+
 }
 
-func waiting(e *fsm.Event, c chan<- string){
+func waiting(e *fsm.Event, c chan<- string) {
 	log.Println("Starting to wait")
 	time.Sleep(2000 * time.Millisecond)
 	log.Println("Waking up")
-	go func(){c<-"wake-up"}()
+	go func() { c <- "wake-up" }()
 }
 
 func main() {
 	/*The main loop is a Finite State Machine.
 	FIXME: draw the machine
 	*/
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	usage := `pmjq
 Usage:
 	pmjq [-C cpu-limit] [-o archive] <spool-dir>
@@ -99,42 +109,43 @@ Options:
 	-C cpu-limit  No jobs are launched if current cpu usage is above limit.
         -o archive    Finished jobs are archived in this directory.
 	`
-
 	arguments, err := docopt.Parse(usage, nil, true, "Poor Man's Job Queue, initial dev version.", false)
 	if err != nil {
 		log.Fatal(err)
 	}
 	spool_dir := arguments["<spool-dir>"].(string)
-	log.SetFlags(log.LstdFlags | log.Lshortfile)
-	
-	
-	//log.Println(arguments)
+	archive_dir := ""
+	if arguments["-o"] != nil {
+		archive_dir = arguments["-o"].(string)
+	}
+
+	log.Println(arguments)
 	event_queue := make(chan string)
 	job_queue := make(chan *os.File)
 	state := fsm.NewFSM(
-	 	"waiting",
-	 	fsm.Events{
-	 		{Name: "wake-up", Src: []string{"waiting"}, Dst: "book-keeping"},
-	 		{Name: "books kept", Src: []string{"book-keeping"}, Dst: "polling"},
-	 		{Name: "job found", Src: []string{"polling"}, Dst: "exec-ing"},
-	 		{Name: "no jobs found", Src: []string{"polling"}, Dst: "waiting"},
-	 		{Name: "launched", Src: []string{"exec-ing"}, Dst: "book-keeping"},
-	 	},
-	 	fsm.Callbacks{
-	 	"book-keeping": func(e *fsm.Event) {book_keeping(e, event_queue)},
-	 	"polling":      func(e *fsm.Event) {polling(e, spool_dir, event_queue, job_queue)},
-		"waiting":      func(e *fsm.Event) {waiting(e, event_queue)},
-		"exec-ing":     func(e *fsm.Event) {execution(e, event_queue, job_queue)},
-	 },
-	 	)
+		"waiting",
+		fsm.Events{
+			{Name: "wake-up", Src: []string{"waiting"}, Dst: "book-keeping"},
+			{Name: "books kept", Src: []string{"book-keeping"}, Dst: "polling"},
+			{Name: "job found", Src: []string{"polling"}, Dst: "exec-ing"},
+			{Name: "no jobs found", Src: []string{"polling"}, Dst: "waiting"},
+			{Name: "launched", Src: []string{"exec-ing"}, Dst: "book-keeping"},
+		},
+		fsm.Callbacks{
+			"book-keeping": func(e *fsm.Event) { book_keeping(e, event_queue) },
+			"polling":      func(e *fsm.Event) { polling(e, spool_dir, event_queue, job_queue) },
+			"waiting":      func(e *fsm.Event) { waiting(e, event_queue) },
+			"exec-ing":     func(e *fsm.Event) { execution(e, archive_dir, event_queue, job_queue) },
+		},
+	)
 	//Event loop
-	go func(){event_queue <- "wake-up"}()
+	go func() { event_queue <- "wake-up" }()
 	cont := true
 	for cont {
 		event := <-event_queue
 		err := state.Event(event)
 		if err != nil {
-		log.Fatalln(err)
+			log.Fatalln(err)
 		}
 	}
 }
