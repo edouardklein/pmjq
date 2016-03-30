ef2c9cc13cf463767b4c4174266312a88d3310fa
Edouard Klein <edouardklein@gmail.com>
2016-03-30 16:42:03 +0200

    WIP on the documentation

--- pmjqtools/tools.py
+++ pmjqtools/tools.py
@@ -2,8 +2,8 @@
 Tools
 ======
 
-``pmjq_interactive``
----------------------
+``pmjq_interactive`` tutorial
+-----------------------------
 
 The ``pmjq_interactive`` tool generates three shell scripts.
  - The first one (``setup.sh``) creates the directories, users and groups and\
@@ -46,13 +46,22 @@ Such a file may be used on the command line with::
 
     grep -v -E '^#' < whole_pipeline.txt | ../pmjq_interactive
 
+
+``pmjq_interactive`` Code organization
+---------------------------------------
+
+If you want to dig in the code, or have a better inderstanding of how this
+tool structures the directory, groups, users and rights, read on.
+
+
 '''
 from collections import namedtuple
 
 Invocation = namedtuple('Invocation', 'inputs,command,outputs,pattern')
+Invocation.__doc__ = 'This data structure represents one invocation of pmjq'
 
 INVOCATIONS = set()
-'''The INVOCATIONS set grows as the user inputs data, it then processed.'''
+'''The INVOCATIONS set grows as the user inputs data, it then is processed.'''
 
 
 def invocation_name(invocation):
@@ -65,3 +74,62 @@ def invocation_name(invocation):
                                        invocation.command +
                                        ' '.join(invocation.outputs))))[:3]
 
+
+def read_user_input():
+    '''Prompt the user, create invocations and return them'''
+    answer = set()
+    while True:
+        command = input('Command:')
+        if command == '':
+            break
+        inputs = tuple(sorted(input('Input dir(s):').split(' ')))
+        pattern = '(.*)'
+        if len(inputs) > 1:
+            pattern = input('Pattern (default "(.*)"):') or pattern
+        outputs = tuple(sorted(input('Output dir(s):').split(' ')))
+        answer.add(Invocation(inputs, command, outputs, pattern))
+    return answer
+
+
+def dir2groups(invocations):
+    '''Return the groups and directories that ought to be created
+
+    First, there is one group per input directory.
+
+    Then, there is one group per global output directory.
+
+    A global output directory is a directory no pmjq invocation reads from,
+    and in which, consequently, data will accumulate (presumably to be used
+    by a human).
+
+    The returned values is a dictionary that maps directory names to the group
+    that own them.
+    '''
+    directories = set(d for i in invocations for d in i.inputs) |\
+        set(d for i in invocations for d in i.outputs)
+    output_dirs = directories - \
+        set(d for i in invocations for d in i.inputs)
+    dir2groups = {d: 'pg_'+d for d in output_dirs}
+    for i in invocations:
+        if len(i.inputs) > 1:
+            for j, d in enumerate(i.inputs):
+                dir2groups[d] = 'pg_'+invocation_name(i)+'_'+str(j)
+            else:
+                dir2groups[i.inputs[0]] = 'pg_'+invocation_name(i)
+    return dir2groups
+
+
+def user2groups(invocations):
+    '''Return the users that ought to be created
+
+    There is one user per pmjq invocation.
+
+    This user belongs to the groups that own their input and output dirs.
+
+    The returned value is a dictionary that maps users to the groups they
+    belong to.
+    '''
+    d2g = dir2groups(invocations)
+    def groups(i):
+        return [d2g[d] for d in ]
+    return {'pu_'+invocation_name(i):[]}
