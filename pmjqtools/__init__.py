c7b8b1fc70af35cce773eea00cadcdeab559f2cf
Edouard Klein <edouardklein@gmail.com>
2016-04-04 17:39:00 +0200

    Make the documentation marginally better

--- pmjqtools/__init__.py
+++ pmjqtools/__init__.py
@@ -1,10 +1,10 @@
-'''The pmjqtools module provides supporting tools for the PMJQ distributed
+'''The pmjqtools module provides supporting tools for the ``pmjq`` distributed
 computing framework.
 
 Basic principle
 ===================
 
-PMJQ is a daemon that monitors one or several input directories. It launches a
+``pmjq`` is a daemon that monitors one or several input directories. It launches a
 process everytime there is data to process in the input directories. The
 process writes its output in one or more output directories.
 
@@ -14,19 +14,19 @@ onther), to compose an arbitrarily complex processing network.
 By distributing the underlying file system, one can distribute the computation
 across several nodes.
 
-The reference document for PMJQ is the `paper
+The reference document for ``pmjq`` is the `paper
 <https://github.com/edouardklein/pmjq/raw/master/paper/main.pdf>`_. It
-gives an overview of PMJQ and its tooling, and demonstrate that PMJQ maps to a
+gives an overview of ``pmjq`` and its tooling, and demonstrate that ``pmjq`` maps to a
 powerful mathematical model of distributed computing: the Petri Nets.
 
-PMJQ concepts
+``pmjq`` concepts
 ==============
 
-PMJQ can be used to build complex processing pipelines, with branching and
+``pmjq`` can be used to build complex processing pipelines, with branching and
 merging.
 
 A `pipeline` is composed of multiple `invocations`. An invocation is a line in
-a script that calls pmjq, which will read its input from some directories and
+a script that calls ``pmjq``, making it read its input from some directories and
 write its output in some others.
 
 Note that there can exist multiple `instances` of this invocation, on multile
@@ -39,6 +39,18 @@ complete data processing workflows, such as this one for video processing:
 
 Installation
 =====================
+
+Tools
+------
+The tooling can be installed with::
+
+    make install
+
+Note that this only needs to be installed on the designer's machine. In order to run a ``pmjq`` pipeline, one only needs the pmjq executable which, being written in Golang, is statically linked and can therefore be copied to the computation nodes directly.
+
+
+Paper
+------
 
 The paper can be build by issuing ``make paper`` in the root directory of the
 project. The paper's figures are not fixed, they are built with the tooling
