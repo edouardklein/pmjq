41e8bdd27bbca1dc0ede7d101eda132c86f769dc
Edouard <edouard.klein@gendarmerie.interieur.gouv.fr>
2017-04-21 15:13:10 +0200

    Remove hy and add daemux as deps.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='pmjqtools',
       version='0.0.1',
-      install_requires=["hy"],
+      install_requires=["daemux"],
       description='Supporting tools for the PMJQ distributed computing\
       framework',
       url='',
@@ -13,7 +13,7 @@ setup(name='pmjqtools',
       package_dir={'pmjqtools':
                    'pmjqtools'},
       include_package_data=True,
-      package_data={"pmjqtools": ["*.hy"]},
+      package_data={},
       entry_points={
           'console_scripts': ['pmjq_interactive=pmjqtools:pmjq_interactive',
                               'pmjq_viz=pmjqtools:pmjq_viz'],
